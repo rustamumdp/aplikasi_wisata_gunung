566cc827310c93954b4030cfa1ad18d930558002
Marcel Antoneo Ananda <marcelananda104@mhs.mdp.ac.id>
2023-12-14 15:27:52 +0700

    merubah title
    judul berubah menjadi "wisata gunung"

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'wisata gunung',
       // tes
       theme: ThemeData(
         // This is the theme of your application.
